1b76fa2352c789a411eca56e2fa3c4a332d21e14
Josefine Klasson <jossanwowar93@gmail.com>
2025-10-10 15:14:16 +0200

    only admin can now make other admin a user only

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getUsers, createUser, loginUser } from "../controllers/userController";
 import { adminOnly, authMiddleware } from "../middelware/auth";
 import  User from "../models/User"
+import { error } from "console";
 
 const router = Router();
 
@@ -29,20 +30,50 @@ router.get("/all", authMiddleware, adminOnly, getUsers);
 router.patch("/:id/make-admin", authMiddleware, adminOnly, async (req, res)=> {
     try {
         const { id } = req.params;
-        const user = await User.findByIdAndUpdate(
-            id,
-            { role: "admin" },
-            { new: true }
-        ).select("-password");
+        const user = await User.findById(id);
 
         if (!user) {
             return res.status(404).json({ error: "Användare hittas inte" });
         }
 
+        if (user.role === "admin") {
+            return res.status(400).json({ error: `${user.name} är redan admin` });
+        }
+
+        user.role = "admin";
+        await user.save();
+
         res.json({ message: `${user.name} är nu admin`, user });
     } catch (error) {
         res.status(500).json({ error: "kunde inte uppdatera användarroll" });
 }
+});
+
+// så en admin kan ta bort admin status från annan
+router.patch("/:id/remove-admin", authMiddleware, adminOnly, async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // hitta användaren
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ error: "användare hittas inte" });
+        }
+
+        // kolla om användaren redan är vanlig user
+        if (user.role === "user") {
+            return res.status(400)
+        }
+
+        // uppdatera roll till user
+        user.role = "user";
+        await user.save();
+
+        res.json({ message: `${user.name} är inte längre admin`, user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "kunde inte uppdatera användarroll" });
+    }
 });
 
 export default router;
