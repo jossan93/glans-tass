5b9ae392a9fc69f5b6d3387f453ad970383abf15
Josefine Klasson <jossanwowar93@gmail.com>
2025-10-20 13:46:08 +0200

    added some text and css to homepage

--- frontend/fronted/src/pages/HomePage.tsx
+++ frontend/fronted/src/pages/HomePage.tsx
@@ -1,3 +1,13 @@
+import "../styles/pages/HomePage.css"
+
 export default function HomePage() {
-    return <h1>start</h1>;
+    return (
+        <div className="home-container">
+            <h1>Välkommen till Glans & Tass</h1>
+            <p>
+                Vi erbjuder professionell hund och kattvård med omtanke, trygghet och kvalitet. Hos oss får din fyrbenta vän den bästa omsorgen i en lugn och trevlig miljö.
+            </p>
+            
+        </div>
+    )
 }
