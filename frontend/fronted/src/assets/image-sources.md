6996fb35d886a272b1aa22ad5418bc1cc52da83d
Josefine Klasson <jossanwowar93@gmail.com>
2025-10-30 23:29:14 +0100

    added fotos to mapp public and added two to homepage, and added a md file with link to where fotos from and who took them

---
+++ frontend/fronted/src/assets/image-sources.md
@@ -0,0 +1,18 @@
+# Bildkällor
+Alla bilder kommer ifrån pexels
+
+## HomePage
+- cat-bluebackground.jpg Photo by Lucas Pezeta: https://www.pexels.com/photo/cute-cat-with-red-and-white-fur-6318994/
+
+two-dogs.jpg Photo by Son Tung Tran: https://www.pexels.com/photo/dog-lying-on-white-surface-6429665/
+## ServiceListPage
+- cat-bath.jpg  — Foto av Karin Chantanaprayura
+https://www.pexels.com/sv-se/foto/djur-husdjur-daggdjur-skarpedjup-7224644/
+
+dog-grooming Photo by Tima Miroshnichenko: https://www.pexels.com/photo/person-grooming-hair-of-yorkshire-terrier-6131569/
+
+## AboutPage
+- big-cat.jpg Photo by Jimme Deknatel: https://www.pexels.com/photo/grey-cat-on-wooden-fence-2518134/
+
+
+dog-on-grass.jpg Photo by Kelemen Boldizsár: https://www.pexels.com/photo/cute-dog-looking-at-the-camera-19024664/
--- frontend/fronted/src/pages/HomePage.tsx
+++ frontend/fronted/src/pages/HomePage.tsx
@@ -1,13 +1,34 @@
-import "../styles/pages/HomePage.css"
+import "../styles/pages/HomePage.css";
 
 export default function HomePage() {
-    return (
-        <div className="home-container">
-            <h1>Välkommen till Glans & Tass</h1>
-            <p>
-                Vi erbjuder professionell hund och kattvård med omtanke, trygghet och kvalitet. Hos oss får din fyrbenta vän den bästa omsorgen i en lugn och trevlig miljö.
-            </p>
-            
+  return (
+    <div className="home-container">
+      <div className="section home-section">
+        <div className="image">
+          <img
+            src="/public/Photos/cat-bluebackground.jpg"
+            alt="katt blå bakgrund"
+            className="hero-cat"
+          />
         </div>
-    )
+
+        <div className="text">
+          <h1>Välkommen till Glans & Tass</h1>
+          <p>
+            Vi erbjuder professionell hund och kattvård med omtanke, trygghet
+            och kvalitet. Hos oss får din fyrbenta vän den bästa omsorgen i en
+            lugn och trevlig miljö.
+          </p>
+        </div>
+
+        <div className="image">
+          <img
+            src="/public/Photos/two-dogs.jpg"
+            alt="två hundar"
+            className="hero-dogs"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
