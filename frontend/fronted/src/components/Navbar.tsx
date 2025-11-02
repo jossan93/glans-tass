523c9154672686778db973bd39bee5e5eefa151d
Josefine Klasson <jossanwowar93@gmail.com>
2025-11-02 17:33:44 +0100

    changed import from big letter to snall on footer and navbar so it is same name as file.

--- frontend/fronted/src/components/Navbar.tsx
+++ frontend/fronted/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import "../styles/Navbar.css"
+import "../styles/navbar.css"
 import { useState, useRef, useEffect } from "react";
 
 export default function Navbar() {
