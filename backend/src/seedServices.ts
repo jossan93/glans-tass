bce9ed8243099429a9381344aaf61028c133cff4
Josefine Klasson <jossanwowar93@gmail.com>
2025-11-04 16:08:37 +0100

    changed the order off seed services simple to more complex and added another service to cat to make it an even 6.

--- backend/src/seedServices.ts
+++ backend/src/seedServices.ts
@@ -7,22 +7,48 @@ dotenv.config();
 const services = [
   // Hundtjänster
   {
+    name: "Kloklippning hund",
+    description: "Klippning av klor på hund.",
+    price: 200,
+    duration: 15,
+    animalType: "hund",
+  },
+  {
+    name: "Puts av päls hund",
+    description:
+      "Putsning av päls på valfritt ställe, t.ex under magen, rumpan eller ansiskte.Enstaka tovor tas bort. Kloklippning ingår.",
+    price: 500,
+    duration: 30,
+    animalType: "hund",
+  },
+  {
+    name: "Tovutredning hund",
+    description:
+      "Försiktig utredning av tovor och borstning utan bad. Kloklippning ingår",
+    price: 600,
+    duration: 60,
+    animalType: "hund",
+  },
+  {
     name: "Hundbad liten",
-    description: "Bad, schamponering, fön och borstning för liten hund.",
+    description:
+      "Bad, schamponering, fön och borstning för liten hund. Kloklippning ingår",
     price: 500,
     duration: 45,
     animalType: "hund",
   },
   {
     name: "Hundbad mellan",
-    description: "Bad, schamponering, fön och borstning för mellanstor hund.",
+    description:
+      "Bad, schamponering, fön och borstning för mellanstor hund. Kloklippning ingår",
     price: 750,
     duration: 60,
     animalType: "hund",
   },
   {
     name: "Hundbad stor",
-    description: "Bad, schamponering, fön och borstning för stor hund.",
+    description:
+      "Bad, schamponering, fön och borstning för stor hund.  Kloklippning ingår",
     price: 900,
     duration: 90,
     animalType: "hund",
@@ -48,34 +74,35 @@ const services = [
     duration: 160,
     animalType: "hund",
   },
-  {
-    name: "Kloklippning hund",
-    description: "klippning av klor på hund.",
-    price: 200,
-    duration: 15,
-    animalType: "hund",
-  },
-  {
-    name: "Puts av päls hund",
-    description:
-      "Putsning av päls på valfritt ställe, t.ex under magen, rumpan eller ansiskte.Enstaka tovor tas bort. Kloklippning ingår.",
-    price: 500,
-    duration: 30,
-    animalType: "hund",
-  },
-  {
-    name: "Tovutredning hund",
-    description:
-      "Försiktig utredning av tovor och borstning utan bad. Kloklippning ingår",
-    price: 600,
-    duration: 60,
-    animalType: "hund",
-  },
 
   // kattjänster
   {
+    name: "Kloklippning katt",
+    description: "Klippning av klor på katt.",
+    price: 200,
+    duration: 15,
+    animalType: "katt",
+  },
+  {
+    name: "Puts av päls katt",
+    description:
+      "Putsning av päls på valfritt ställe, t.ex under magen, rumpan, ansikte. Enstaka tovor tas bort. Kloklippning ingår",
+    price: 500,
+    duration: 30,
+    animalType: "katt",
+  },
+  {
+    name: "Tovutredning katt",
+    description:
+      "Försiktig utredning av tovor och borstning utan bad. Kloklippning ingår",
+    price: 500,
+    duration: 30,
+    animalType: "katt",
+  },
+  {
     name: "Kattbad",
-    description: "Bad, schamponering, fön och borstning för katt.",
+    description:
+      "Bad, schamponering, fön och borstning för katt. Kloklippning ingår",
     price: 600,
     duration: 60,
     animalType: "katt",
@@ -89,26 +116,11 @@ const services = [
     animalType: "katt",
   },
   {
-    name: "Kloklippning katt",
-    description: "Klippning av klor på katt.",
-    price: 200,
-    duration: 15,
-    animalType: "katt",
-  },
-  {
-    name: "Puts av päls katt",
+    name: "Kattbad och lejonklippning",
     description:
-      "Putsning av päls på valfritt ställe, t.ex under magen, rumpan, ansiskte. Enstaka tovor tas bort. Kloklippning ingår",
-    price: 500,
-    duration: 30,
-    animalType: "katt",
-  },
-  {
-    name: "Tovutredning katt",
-    description:
-      "Försiktig utredning av tovor och borstning utan bad. Kloklippning ingår",
-    price: 500,
-    duration: 30,
+      "Bad, schamponering, fön och borstning för katt och lejonklippning. Kloklippning ingår",
+    price: 900,
+    duration: 75,
     animalType: "katt",
   },
 ];
